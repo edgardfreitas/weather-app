871967ffa3da813958709b0f583c58ddaaa2e464
edgardfreitas <edgardfp@gmail.com>
2025-03-25 16:49:58 -0300

    update README.md

--- index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-function citySearch() {
-  axios.get("http://dataservice.accuweather.com/locations/v1/search")
-  .then(response => console.log(response))
-  .catch(error => console.log(error))
-}
-
-citySearch();
--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+import cors from 'cors';
 import express from 'express';
-import city from '.routes/city';
+import axios from 'axios';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (request, response) => response.status(200).json({
+app.get('/health', (request, response) => response.status(200).json({
   message: "OK!"
 }));
 
-app.use('/profile', city);
+app.get('/', async (request, response) => {
+
+  try {
+    // response é a resposta do axios MAS eu tiro o data de dentro do response
+    const { data } = await axios('http://dataservice.accuweather.com/locations/v1/search?q=riodejaneiro&apikey={vLLkxz7OYYLm4ybXF7FElwIP0PGlWNVa}')
+
+    return response.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+  
+});
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Executando o servidor na porta ${PORT}`));
